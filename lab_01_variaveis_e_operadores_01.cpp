ef7adc984118afc65d099982f4d0ed7fc8821591
Aldglyr Dias <aldglyr@outlook.com>
2025-04-02 15:51:11 -0300

    questao 01 da 1ra lista

---
+++ lab_01_variaveis_e_operadores_01.cpp
@@ -0,0 +1,17 @@
+/*
+Escreva um programa que:
+1. Atribui o valor 57 a uma variável n do tipo inteiro
+2. Imprime n, o quadrado de n e o cubo de n
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int inteiro = 57;
+    cout << inteiro << endl;
+    cout << pow(inteiro,2) << endl;
+    cout << pow(inteiro,3) << endl;
+    return 0;
+}
